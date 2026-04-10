38f73311302d1e634df173d3b489c3988288e3c7
Slawomir Pacek <slawomir_pacek@op.pl>
2026-04-10 13:51:12 +0200

    fruitShop8

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,10 +1,23 @@
 package core.basesyntax;
 
-import core.basesyntax.io.*;
+import core.basesyntax.io.CsvFileReader;
+import core.basesyntax.io.CsvFileReaderImpl;
+import core.basesyntax.io.FileWriter;
+import core.basesyntax.io.FileWriterImpl;
 import core.basesyntax.model.FruitTransaction;
-import core.basesyntax.service.*;
-import core.basesyntax.service.impl.*;
-
+import core.basesyntax.service.DataConverter;
+import core.basesyntax.service.OperationHandler;
+import core.basesyntax.service.OperationStrategy;
+import core.basesyntax.service.ShopService;
+import core.basesyntax.service.impl.BalanceOperation;
+import core.basesyntax.service.impl.DataConverterImpl;
+import core.basesyntax.service.impl.PurchaseOperation;
+import core.basesyntax.service.impl.ReturnOperation;
+import core.basesyntax.service.impl.SupplyOperation;
+import core.basesyntax.service.impl.OperationStrategyImpl;
+import core.basesyntax.service.impl.ShopServiceImpl;
+import core.basesyntax.service.impl.ReportGeneratorImpl;
+import core.basesyntax.service.ReportGenerator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,14 +27,11 @@ public class Main {
     public static void main(String[] args) {
 
         CsvFileReader reader = new CsvFileReaderImpl();
-        FileWriter writer = new FileWriterImpl();
 
         List<String> input =
                 reader.read("src/main/resources/reportToRead.csv");
 
         DataConverter converter = new DataConverterImpl();
-        List<FruitTransaction> transactions =
-                converter.convertToTransaction(input);
 
         Map<FruitTransaction.Operation, OperationHandler> handlers = new HashMap<>();
         handlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
@@ -31,6 +41,8 @@ public class Main {
 
         OperationStrategy strategy = new OperationStrategyImpl(handlers);
 
+        List<FruitTransaction> transactions =
+                converter.convertToTransaction(input);
         ShopService shopService = new ShopServiceImpl(strategy);
         shopService.process(transactions);
 
@@ -39,6 +51,7 @@ public class Main {
 
         String report = reportGenerator.getReport();
 
+        FileWriter writer = new FileWriterImpl();
         writer.write(report, "src/main/resources/finalReport.csv");
     }
 }
